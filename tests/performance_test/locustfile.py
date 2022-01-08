30384da1f46e32c4ae01c940f3991a6ab125067d
Valentin Pheulpin <Valentin.Pheulpin.Pro@gmail.com>
2022-01-08 12:03:36 +0100

    First version

---
+++ tests/performance_test/locustfile.py
@@ -0,0 +1,36 @@
+from locust import HttpUser, task
+
+
+class ProjectPerfTest(HttpUser):
+
+    def on_start(self):
+        self.client.get("/")
+
+    def on_stop(self):
+        self.client.get("/logout")
+        
+    @task
+    def index(self):
+        self.client.get("/")
+
+    @task
+    def table(self):
+        self.client.get("/table")
+
+    @task
+    def showsummary(self):
+        self.client.post(
+            '/showSummary', data={'email': 'john@simplylift.co'})
+        
+    @task
+    def book(self):
+        self.client.get('/book/Fall Classic/Simply Lift')
+
+    @task
+    def purchasePlaces(self):
+        self.client.post(
+            '/purchasePlaces', data={'club': 'Simply Lift', 'competition': "Fall Classic", 'places': 3})
+
+    @task
+    def logout(self):
+        self.client.get('/logout')
